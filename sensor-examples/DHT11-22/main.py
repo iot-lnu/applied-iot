05a9f683c306b0ab91a85321e3d4d9372106ca36
Fredrik Ahlgren <fredrik@ahlgren.nu>
2020-06-26 15:45:30 -0700

    update DHT lib, correct spelling

--- sensor-examples/DHT11-22/main.py
+++ sensor-examples/DHT11-22/main.py
@@ -1,14 +1,13 @@
 # Data is sent to Pybytes. Needs to flashed with Pybyte firmware
 import time
 from machine import Pin
-from dth import DHT # https://github.com/JurassicPork/DHT_PyCom
+from dht import DHT # https://github.com/JurassicPork/DHT_PyCom
 
 # Type 0 = dht11
 # Type 1 = dht22
 
-th = DHT(Pin('P23', mode=Pin.OPEN_DRAIN), 1)
+th = DHT(Pin('P23', mode=Pin.OPEN_DRAIN), 0)
 time.sleep(2)
-
 
 while True:
     result = th.read()
@@ -17,7 +16,7 @@ while True:
         result = th.read()
     print('Temp:', result.temperature)
     print('RH:', result.humidity)
-    pybytes.send_signal(1,result.temperature)
-    pybytes.send_signal(2,result.humidity)
+    #pybytes.send_signal(1,result.temperature)
+    #pybytes.send_signal(2,result.humidity)
 
     time.sleep(5)
