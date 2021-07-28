4f638845fb87d914826fd796d8671a62af0c98bb
David M <david.albazii@gmail.com>
2021-07-28 18:11:27 +0200

    removed some inaccurate/old comments

--- sensor-examples/Grove Oled 1"2 display 128x128/main.py
+++ sensor-examples/Grove Oled 1"2 display 128x128/main.py
@@ -1,12 +1,9 @@
 import ssd1327
 
-
-# or using Hardware I2C
 from machine import I2C, Pin
-i2c = I2C(0) # TinyPICO sda=19, scl=18
+i2c = I2C(0)
 
-#display = ssd1327.WS_OLED_128X128(i2c)  # Grove OLED Display
-display = ssd1327.SH1107_I2C(128, 128, i2c)  # WaveShare, Zio Qwiic
+display = ssd1327.SH1107_I2C(128, 128, i2c)
 
 display.text('Hello World', 20, 20, 255)
 display.show()
