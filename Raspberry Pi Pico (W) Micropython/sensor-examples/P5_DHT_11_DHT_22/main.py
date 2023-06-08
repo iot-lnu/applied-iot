7c6360482a029d83c032f02f0bcf065b7daa4044
David M <dmozartandraws@gmail.com>
2023-06-08 18:14:05 +0200

    updated dht lib & pin

--- Raspberry Pi Pico (W) Micropython/sensor-examples/P5_DHT_11_DHT_22/main.py
+++ Raspberry Pi Pico (W) Micropython/sensor-examples/P5_DHT_11_DHT_22/main.py
@@ -1,10 +1,10 @@
 from machine import Pin
 import utime as time
-from dht import DHT11, InvalidChecksum
+from dht import DHT11
 
 while True:
     time.sleep(5)
-    pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
+    pin = Pin(27, Pin.OUT, Pin.PULL_DOWN)
     sensor = DHT11(pin)
     t  = (sensor.temperature)
     h = (sensor.humidity)
