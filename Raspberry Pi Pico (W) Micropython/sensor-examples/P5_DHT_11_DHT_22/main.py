74bc17017ba98fe293aa790b9636a57c040f6b2c
David M <dmozartandraws@gmail.com>
2023-06-08 18:19:11 +0200

    moved out object creation from the loop

--- Raspberry Pi Pico (W) Micropython/sensor-examples/P5_DHT_11_DHT_22/main.py
+++ Raspberry Pi Pico (W) Micropython/sensor-examples/P5_DHT_11_DHT_22/main.py
@@ -1,12 +1,16 @@
 from machine import Pin
 import utime as time
 from dht import DHT11
+import struct 
+
+pin = Pin(27, Pin.OUT, Pin.PULL_DOWN)
+sensor = DHT11(pin)
 
 while True:
     time.sleep(5)
-    pin = Pin(27, Pin.OUT, Pin.PULL_DOWN)
-    sensor = DHT11(pin)
+
     t  = (sensor.temperature)
     h = (sensor.humidity)
-    print("Temperature: {}".format(sensor.temperature))
-    print("Humidity: {}".format(sensor.humidity))
+    
+    print("Temperature: {}".format(t))
+    print("Humidity: {}".format(h))
